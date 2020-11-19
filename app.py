aa76e576e5c340a6b919fc346ea156f59f75e6f8
Sampson <boamah.sampson34@gmail.com>
2020-11-19 10:04:26 +0000

    Anything can go. we move

---
+++ app.py
@@ -0,0 +1,64 @@
+print ('Sampson is learning python')
+print ('I am new with AWS')
+
+x = 10 
+y = 'its almost time'
+z = True
+a = 10.5
+print(type (x))
+print(type (y))
+print(type (z))
+print(type (a))
+
+firstnumber = 50
+secondnumber = 100
+total = firstnumber + secondnumber 
+print(total)
+
+# Multipication
+
+num1 = 20
+num2 = 50
+total = num1 * num2
+print(total)
+# Addition
+num1 = 50
+num2 = 15
+total = num1 + num2
+print(total)
+
+#division
+
+num1 = 100
+num2 = 50
+total = num1 / num2
+print(total)
+
+
+#Exponent
+octect = (2 ** 7) -1
+print(octect)
+
+#Modulo operater
+dulo = 4 % 3
+print(dulo)
+dulo = 9 % 6
+print(dulo)
+
+
+#String
+school = "University of Ghana"
+print(school)
+
+
+print(len)(school)
+print(school)
+
+school= """""" I am a student of the 
+university of Ghana
+I am in level 400 """
+print(school)
+
+
+
+
